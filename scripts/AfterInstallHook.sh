c8cf4d2ff5663ac4d16614e0ffd696799b8ae26a
Ubuntu <ubuntu@ip-10-0-1-50.ec2.internal>
2021-03-04 23:23:34 +0000

    fixes

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -5,6 +5,5 @@ npm install
 echo "modules installed"
 if pgrep nodejs; then pkill nodejs; fi
 echo "nodejs processes killed"
-nodejs ./app.js /dev/null 2> /dev/null < /dev/null &
+nodejs ./app.js > /dev/null 2> /dev/null < /dev/null &
 echo "app started"
-exit 0
